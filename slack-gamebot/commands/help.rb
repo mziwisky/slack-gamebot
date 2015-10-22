2127361f8e79fba3db3aefcac577190d68bb5ce3
Michael Ziwisky <mziwisky@instructure.com>
2015-10-22 16:00:57 -0500

    damn, forgot a quote

--- slack-gamebot/commands/help.rb
+++ slack-gamebot/commands/help.rb
@@ -6,7 +6,7 @@ module SlackGamebot
         message = <<-HELP_MSG
 I understand these commands:
 ```#{commands.join("\n")}```
-See https://github.com/dblock/slack-gamebot for details."
+See https://github.com/dblock/slack-gamebot for details.
         HELP_MSG
         send_message_with_gif client, data.channel, message, 'help'
         logger.info "HELP: #{data.user}"
